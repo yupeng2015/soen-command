74ecfd3d73338beb4ff4b6e80d28cf33012cc9d1
soen <970852318@qq.com>
2020-05-28 22:23:08 +0800

    feat:symfony /console

--- src/Main.php
+++ src/Main.php
@@ -11,12 +11,9 @@ class Main
 {
     function __construct($httpConfigDirectory)
     {
-        $array = new Container($httpConfigDirectory);
-//        var_dump($array);
+
     }
     
     public function run(){
-        \App::getComponent('router')->test();
-//        context()->getComponent('router')->test();
     }
 }

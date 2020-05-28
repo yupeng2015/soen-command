9a3350c382ae7dd5f6c9169de1dfcc81a510e17f
soen <970852318@qq.com>
2020-05-28 17:29:55 +0800

    feat

--- src/Main.php
+++ src/Main.php
@@ -4,7 +4,17 @@
 namespace Soen\Command;
 
 
+use Soen\Container\Server;
+
 class Main
 {
-
+    function __construct($httpConfigDirectory)
+    {
+        $array = (new Server($httpConfigDirectory))->load();
+//        var_dump($array);
+    }
+    
+    public function run(){
+        context()->getComponent('router')->test();
+    }
 }
